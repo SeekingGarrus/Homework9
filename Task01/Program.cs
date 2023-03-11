fbbb805830bdfda7794ddbc8fe2d2dc9d3a0a27d
@Olga.shka@ <shkarina.ol@yandex.ru>
2023-03-11 16:40:38 +0400

    Initial commit

---
+++ Task01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int Number(string message) // ввод числа N
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool ValidateNumber(int number) // функция bool для проверки на положительность
+{
+    if (number < 1)
+    {
+        Console.WriteLine("Это не положительное число!");
+        return false;
+    }
+    return true;
+}
+
+void PrintNaturalNum(int n) //вывод с рекурсией
+{
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNaturalNum(n - 1);
+}
+
+
+int n = Number("Введите любое положительное число: ");
+ValidateNumber(n);
+PrintNaturalNum(n);
---
+++ Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int GetNumber(string text) // функция для ввода числа
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int NaturalSum(int m, int n) // функция нахождения суммы с рекурсией
+{
+    if (m == n || m < 0 || n < 0)
+        return n;
+    return n + NaturalSum(m, n - 1);
+}
+
+
+void ValidateNumber(int m, int n) // метод для проверки на положительность и вывод
+{
+    if (m < 0 || n < 0) Console.WriteLine("Это не положительное число!");
+    else Console.WriteLine($"Сумма элементов в промежутке {m} до {n} включительно: {NaturalSum(m, n)}");
+}
+
+
+int m = GetNumber("Введите положительное число M: ");
+int n = GetNumber("Введите положительное число N: ");
+NaturalSum(m, n);
+ValidateNumber(m, n);
